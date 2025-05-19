d0d4cf48e75db54c125b66e8ff04b5893620dbf6
ErnestoDomenech <ernestodomenech@live.com>
2025-05-19 11:52:29 +0000

    Add Admin entities

---
+++ BaseLibrary/Entities/AdminProfile.cs
@@ -0,0 +1,11 @@
+﻿namespace BaseLibrary.Entities
+{
+    public class AdminProfile
+    {
+        public int AdministratorId { get; set; }
+        public int CivilId { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public virtual Administrator? Administrator { get; set; }
+    }
+}
---
+++ BaseLibrary/Entities/Administrator.cs
@@ -0,0 +1,8 @@
+﻿
+namespace BaseLibrary.Entities
+{
+    public class Administrator: BaseEntity
+    {
+        public virtual AdminProfile? Profile { get; set; }
+    }
+}
